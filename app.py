bb0b8f83338afb50b34a6df2a682ee20b9ef851f
Nikita-Zakh <zakhvatoshin.nikita@gmail.com>
2026-02-21 14:03:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from services.google_service import get_google_results
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # ← КРИТИЧНО!
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -9,14 +10,21 @@ def home():
     results = []
     if request.method == "POST":
         query = request.form.get("query", "")
-        results = get_google_results(query)
+        try:
+            results = get_google_results(query)
+        except:
+            results = []
     return render_template("index.html", query=query, results=results)
 
 @app.route("/download")
 def download():
     query = request.args.get("query", "")
-    results = get_google_results(query)
+    try:
+        results = get_google_results(query)
+    except:
+        results = []
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
